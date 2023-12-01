613ffa2b768f44b77f4b68e7762e683fb3010268
Jonas Samuelsson <jonassamuelsson@outlook.com>
2023-12-01 20:44:59 +0100

    day 1

---
+++ 2023/AdventOfCode2023.Day01/Program.cs
@@ -0,0 +1,19 @@
+﻿var sum = File.ReadAllLines("input.txt")
+   .Select(s => s.Trim())
+   .Select(s => GetDigit(s) + GetDigit(string.Join("", s.Reverse())))
+   .Select(int.Parse)
+   .Sum();
+
+Console.WriteLine(sum);
+
+string GetDigit(string s)
+{
+   for (var i = 0; i < s.Length; i++)
+   {
+      var c = s[i];
+      if (!char.IsDigit(c)) continue;
+      return c.ToString();
+   }
+
+   throw new InvalidOperationException();
+}
