63350ad577f7d9d386f302664a61e0a5fa6ea787
Jonas Samuelsson <jonassamuelsson@outlook.com>
2023-12-01 21:38:36 +0100

    day 1 part 2

--- 2023/AdventOfCode2023.Day01/Program.cs
+++ 2023/AdventOfCode2023.Day01/Program.cs
@@ -1,19 +1,82 @@
 ﻿var sum = File.ReadAllLines("input.txt")
    .Select(s => s.Trim())
-   .Select(s => GetDigit(s) + GetDigit(string.Join("", s.Reverse())))
+   .Select(s => GetFirstDigit(s) + GetLastDigit(s))
    .Select(int.Parse)
    .Sum();
 
 Console.WriteLine(sum);
 
-string GetDigit(string s)
+string GetFirstDigit(string s)
 {
    for (var i = 0; i < s.Length; i++)
    {
-      var c = s[i];
-      if (!char.IsDigit(c)) continue;
-      return c.ToString();
+      if (!TryGetValue(s, i, out var value)) continue;
+      return value;
    }
 
    throw new InvalidOperationException();
 }
+
+string GetLastDigit(string s)
+{
+   for (var i = s.Length - 1; i >= 0; i--)
+   {
+      if (!TryGetValue(s, i, out var value)) continue;
+      return value;
+   }
+
+   throw new InvalidOperationException();
+}
+
+bool TryGetValue(string s, int index, out string value)
+{
+   value = "";
+
+   foreach (var v in GetValues())
+   {
+      if (!s.Substring(index).StartsWith(v))
+         continue;
+
+      value = int.TryParse(v, out var i)
+         ? i.ToString()
+         : v switch
+         {
+            "one" => "1",
+            "two" => "2",
+            "three" => "3",
+            "four" => "4",
+            "five" => "5",
+            "six" => "6",
+            "seven" => "7",
+            "eight" => "8",
+            "nine" => "9",
+            _ => throw new Exception(v)
+         };
+
+      return true;
+   }
+
+   return false;
+}
+
+string[] GetValues() => new[]
+{
+   "1",
+   "2",
+   "3",
+   "4",
+   "5",
+   "6",
+   "7",
+   "8",
+   "9",
+   "one",
+   "two",
+   "three",
+   "four",
+   "five",
+   "six",
+   "seven",
+   "eight",
+   "nine"
+};
